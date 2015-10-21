5d906e805f162f47ccc1fc11794e2f299df5a0ec
Tiago Mendes-Costa <tiago@mendes-costa.net>
2015-10-21 11:38:11 +0200

    Add optional link to events

--- config.rb
+++ config.rb
@@ -41,11 +41,19 @@
 # end
 
 # Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+  def link_to_if(condition, name, options = {}, html_options = {}, &block)
+    if condition
+      link_to(name, options, html_options)
+    else
+      if block_given?
+        block.arity <= 1 ? capture(name, &block) : capture(name, options, html_options, &block)
+      else
+        ERB::Util.html_escape(name)
+      end
+    end
+  end
+end
 
 set :relative_links, true
 
